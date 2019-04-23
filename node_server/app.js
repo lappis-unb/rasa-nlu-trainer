72a506cf0d08d99243480eccb173a24e550eb161
Ygor Galeno <ygor.tgaleno@gmail.com>
2019-04-23 11:06:21 -0300

    Enabling CORS

--- node_server/app.js
+++ node_server/app.js
@@ -5,11 +5,21 @@ const cors = require('cors');
 const index = require('./routes/index');
 
 const app = express();
+const whitelist = [process.env.URL_GITHUB_IO];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use(index);
 module.exports = app;
